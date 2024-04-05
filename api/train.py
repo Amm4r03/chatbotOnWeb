9471e79426bf635a3c1f220bc398b3d4e2ac4d94
ammar <akidwai03@gmail.com>
2024-04-05 20:01:11 +0530

    update train.py

--- api/train.py
+++ api/train.py
@@ -6,8 +6,8 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
-from api.nltk_utils import bag_of_words, tokenize, stem
-from api.model import NeuralNet
+from nltk_utils import bag_of_words, tokenize, stem
+from model import NeuralNet
 
 with open('intents.json', 'r') as f:
     intents = json.load(f)
