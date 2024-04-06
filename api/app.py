241ec1de879f77748a1e0c34a2f3f00cda9074ef
ammar <akidwai03@gmail.com>
2024-04-06 08:27:10 +0530

    update app.py

--- api/app.py
+++ api/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from chat import get_response, bot_name
+import os
 
 app = Flask(__name__, static_folder='../static', template_folder='../templates')
 
@@ -10,15 +11,23 @@ app.config['SECRET_KEY'] = 'your_secret_key_here'
 # Print configuration options
 print(app.config)
 
+# Handle UnicodeDecodeError
+@app.errorhandler(UnicodeDecodeError)
+def handle_unicode_decode_error(error):
+    return "An error occurred while decoding the request data.", 500
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/send_message', methods=['POST'])
 def send_message():
-    user_message = request.form['message']
-    bot_message = get_response(user_message)
-    return jsonify({'response': bot_message})
+    try:
+        user_message = request.form['message']
+        bot_message = get_response(user_message)
+        return jsonify({'response': bot_message})
+    except UnicodeDecodeError:
+        return jsonify({'response': 'An error occurred while decoding the request data.'}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
